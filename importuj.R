9bf5f148eb851e497e80e2f93f80fedf480f2146
Kacper Pietraszewski <k.pietraszewski@hotmail.com>
2016-11-12 20:34:21 +0100

    Ustalona funkcja: przelicz

--- importuj.R
+++ importuj.R
@@ -29,52 +29,6 @@ przygotowanie <- rep(0, dim(dane)[1])
 przygotowanie = replace(przygotowanie, posilki[,3]==1, 0.5)
 przygotowanie = replace(przygotowanie, posilki[,1]==2, 1)
 
-czestotliwosc <- matrix(c("2-3 razy dziennie", 2.5,
-                  "1 raz dziennie", 1,
-                  "1 w miesiącu", 1/30,
-                  "1 w tygodniu", 4/30,
-                  "2-3 razy w miesiącu", 2.5/30,
-                  "2-3 razy w tygodniu", 4*2.5/30,
-                  "4-5 razy w tygodniu", 4*4.5/30,
-                  "6 razy w tygodniu", 4*6/30,
-                  "Nigdy", 0,
-                  "Rzadziej niż 1 raz w miesiącu", 1/90), ncol=2, byrow = T)
-czestotliwosc = as.data.frame(czestotliwosc)
-
-# przelicz("2-3 razy dziennie", 2.5)
-# przelicz("1 raz dziennie", 1)
-# przelicz("1 w miesiącu", 1/30)
-# przelicz("1 w tygodniu", 4/30)
-# przelicz("2-3 razy w miesiącu", 2.5/30)
-# przelicz("2-3 razy w tygodniu", 4*2.5/30)
-# przelicz("4-5 razy w tygodniu", 4*4.5/30)
-# przelicz("6 razy w tygodniu", 4*6/30)
-# przelicz("Nigdy", 0)
-# przelicz("Rzadziej niż 1 raz w miesiącu", 1/90)
-# przelicz("Jedna kulka puree", 90)
-# przelicz("Jeden cały ziemniak", 85)
-# przelicz("1 cały", 170)
-# przelicz("1 ćwiartka", 170/4)
-# przelicz("1 plaster", 20)
-# przelicz("1 połowa", 170/2)
-# przelicz("1 pomidor koktajlowy", 20)
-# przelicz("świderki", 170/2)
-# przelicz("muszelki", 170/2)
-# przelicz("spaghetti", 170/2)
-# przelicz("kokardki", 170/2)
-# przelicz("rurki", 170/2)
-# przelicz("wstążki", 170/2)
-# przelicz("nitki", 170/2)
-# przelicz("Kukurydziane", 30)
-# przelicz("Miodowe", 30)
-# przelicz("Czekoladowe", 40)
-# przelicz("Cynamonowe", 30)
-# przelicz("Owsiane", 50)
-# przelicz("Muesli", 50)
-# przelicz("Granola", 60)
-# przelicz("Nie spożywam", 0)
-# przelicz("Nie kupuję", 0)
-
 #spozycie = dane[,68]*dane[,69]*dane[,70]
 #spozycie = cbind(spozycie, dane[,98]*dane[,99]*dane[,100])
 #spozycie = cbind(spozycie, 180*dane[,125])
--- przelicz.R
+++ przelicz.R
@@ -1,3 +1,47 @@
-przelicz <- function(x,y){
-  dane[!is.na(dane) & dane == x] = y
+przelicz <- function(kolumna){
+  as.vector(wartosci[dane[,kolumna]])
 }
+wartosci <- c("2-3 razy dziennie" = 2.5,
+              "1 raz dziennie" = 1,
+              "1 w miesiącu" = 1/30,
+              "1 w tygodniu" = 4/30,
+              "2-3 razy w miesiącu" = 2.5/30,
+              "2-3 razy w tygodniu" = 4*2.5/30,
+              "4-5 razy w tygodniu" = 4*4.5/30,
+              "6 razy w tygodniu" = 4*6/30,
+              "Nigdy" = 0,
+              "Rzadziej niż 1 raz w miesiącu" = 1/90)
+
+# przelicz("2-3 razy dziennie", 2.5)
+# przelicz("1 raz dziennie", 1)
+# przelicz("1 w miesiącu", 1/30)
+# przelicz("1 w tygodniu", 4/30)
+# przelicz("2-3 razy w miesiącu", 2.5/30)
+# przelicz("2-3 razy w tygodniu", 4*2.5/30)
+# przelicz("4-5 razy w tygodniu", 4*4.5/30)
+# przelicz("6 razy w tygodniu", 4*6/30)
+# przelicz("Nigdy", 0)
+# przelicz("Rzadziej niż 1 raz w miesiącu", 1/90)
+# przelicz("Jedna kulka puree", 90)
+# przelicz("Jeden cały ziemniak", 85)
+# przelicz("1 cały", 170)
+# przelicz("1 ćwiartka", 170/4)
+# przelicz("1 plaster", 20)
+# przelicz("1 połowa", 170/2)
+# przelicz("1 pomidor koktajlowy", 20)
+# przelicz("świderki", 170/2)
+# przelicz("muszelki", 170/2)
+# przelicz("spaghetti", 170/2)
+# przelicz("kokardki", 170/2)
+# przelicz("rurki", 170/2)
+# przelicz("wstążki", 170/2)
+# przelicz("nitki", 170/2)
+# przelicz("Kukurydziane", 30)
+# przelicz("Miodowe", 30)
+# przelicz("Czekoladowe", 40)
+# przelicz("Cynamonowe", 30)
+# przelicz("Owsiane", 50)
+# przelicz("Muesli", 50)
+# przelicz("Granola", 60)
+# przelicz("Nie spożywam", 0)
+# przelicz("Nie kupuję", 0)
